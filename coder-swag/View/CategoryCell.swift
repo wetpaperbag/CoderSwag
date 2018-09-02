365c2e477413024cf08266b2d4e2d3f8a234fa6a
Darrel Jones <darreljones@Darrels-MacBook-Pro.local>
2018-09-01 21:24:02 -0700

    made main VC files, es, started table view, statered category labels

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Darrel Jones on 9/1/18.
+//  Copyright © 2018 Darrel Jones. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
