efe9074973b5ea4e74b367087f7f906713398eb3
Darrel Jones <darreljones@Darrels-MacBook-Pro.local>
2018-10-02 03:59:46 -0700

    Built out collection view cells, wired up cells

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Darrel Jones on 10/2/18.
+//  Copyright © 2018 Darrel Jones. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UIlabel!
+    
+}
